7dd40974b715f2173d2ff9af6d645133f9d0cb82
1sun12 <regan_sun12@outlook.com>
2024-02-12 14:53:07 -0600

    scanner header file setup complete. Enumeration for different operations, set for tokens, and a scanner class with private and public functions I might end up using or needing.

--- scanner.h
+++ scanner.h
@@ -2,14 +2,48 @@
 #define SCANNER_H
 
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <unordered_set>
+
+enum TokenType {
+    TOKEN_IDENTIFIER,
+    TOKEN_NUMBER,
+    TOKEN_OPERATOR,
+    TOKEN_ASSIGNMENT,
+    TOKEN_BEGIN,
+    TOKEN_END,
+    TOKEN_COMMENT,
+    TOKEN_UNKNOWN,
+    TOKEN_END_OF_FILE
+};
+
+struct Token {
+    TokenType type;
+    std::string lexeme;
+    int line;
+};
 
 class Scanner {
 private:
-    int x;
+    std::ifstream inputFile; // source code file that we are scanning and turning into tokens
+    int lineNumber; // what line we are on in the inputFile
+    std::unordered_set<char> operators = {'+', '-', '*', '/', '=', '(', ')', ';'}; // very similar to dictionaries in python, set of all operations
+    std::unordered_set<std::string> keywords = {"begin", "end"}; // very similar to dictionaries in python, detect if the file is at the top or bottom
+    bool isComment = false; // is token a comment? = completely ignore it
+
 
 public:
-    Scanner();
-    void helloWorld();
+    Scanner(const std::string& inputFile); // constructor that takes in input file address
+    Token getNextToken(); // return the next token in order
+    std::vector<Token> getAllTokens(); // return all tokens, mostly for testing
+
+    char getNextChar();
+    Token scanIdentifierOrKeyword(char firstChar);
+    Token scanNumber(char firstChar);
+    Token scanOperator(char firstChar);
+    Token scanComment(char firstChar);
 };
 
 #endif
