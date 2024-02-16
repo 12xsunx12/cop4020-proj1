f73183614063f7d7f3024a1f2ccbcfed287ff4c7
1sun12 <regan_sun12@outlook.com>
2024-02-16 08:41:56 -0600

    STARTING OVER COMPLETELY: Got lost, confused, and need a clean slate again. Treat this as commit 0 and commits before this as -1,-2,-3...

--- main.cpp
+++ main.cpp
@@ -1,14 +1,9 @@
 #include "scanner.h"
 
 int main() {
-    Scanner s("example-source-code-inputs/a1");
+    Scanner s;
 
-    std::vector<Token> allTokens = s.getAllTokens();
-
-    for (long unsigned int i = 0; i < allTokens.size(); i++) {
-        const Token& token = allTokens[i];
-        std::cout << "Type: " << token.type << ", Lexeme: " << token.lexeme << ", Line: " << token.lineNumber << std::endl;
-    }
+    std::cout << s.print();
 
     return 0;
 }
--- scanner.h
+++ scanner.h
@@ -2,48 +2,13 @@
 #define SCANNER_H
 
 #include <iostream>
-#include <fstream>
-#include <string>
-#include <vector>
-#include <unordered_set>
-#include <cctype>
-
-enum TokenType {
-    TOKEN_IDENTIFIER,
-    TOKEN_NUMBER,
-    TOKEN_OPERATOR,
-    TOKEN_ASSIGNMENT,
-    TOKEN_BEGIN,
-    TOKEN_END,
-    TOKEN_COMMENT,
-    TOKEN_UNKNOWN, // token was not recognized when scanned
-    TOKEN_END_OF_FILE
-};
-
-struct Token {
-    TokenType type;
-    std::string lexeme;
-    int lineNumber;
-};
 
 class Scanner {
-private:
-    std::ifstream inputFile; // source code file that we are scanning and turning into tokens
-    int lineNumber; // what line we are on in the inputFile
-    std::unordered_set<char> operators = {'+', '-', '*', '/', '=', '(', ')', ';'}; // very similar to dictionaries in python, set of all operations
-    std::unordered_set<std::string> keywords = {"begin", "end"}; // very similar to dictionaries in python, detect if the file is at the top or bottom
-    bool isComment = false; // is token a comment? = completely ignore it
+    private:
+        std::string test = "hello";
 
-    char getNextChar(); // retreive next lexeme in sequence of characters
-    Token scanIdentifierOrKeyword(char firstChar); // is this 'begin' or 'end' or something else
-    Token scanNumber(char firstChar); // is this a number?
-    Token scanOperator(char firstChar); // is this an operator?
-    Token scanComment(char firstChar); // is this a comment?
-
-public:
-    Scanner(const std::string& fileName); // constructor that takes in input file address
-    Token getNextToken(); // return the next token in order
-    std::vector<Token> getAllTokens(); // return all tokens, mostly for testing
+    public:
+        std::string print();
 };
 
 #endif
